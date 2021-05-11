ae3ff1592ee3ed0c10f06a504363a0f68a77dc98
Bellagrunt <mattbellantoni3@gmail.com>
2021-05-11 15:22:16 -0400

    public

--- cryptozombies/Contract.sol
+++ cryptozombies/Contract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity  >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
@@ -14,11 +14,16 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
-    }
+    } 
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
